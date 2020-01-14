e64d12327efe24652534301efe300917991443cc
Victor-28 <victorakintola@gmail.com>
2020-01-14 16:33:57 +0100

    weather forecast

--- scriptweather.js
+++ scriptweather.js
@@ -6,6 +6,7 @@ let cityName = "";
 
 document.getElementById("getWeatherForecast").addEventListener("click", function() {
     cityName = document.getElementById("city").value;
+    console.log(cityName);
 
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=01951a6e2ef5df48e90ed4c64b1234fc&units=metric`)
         .then(function (response) {
@@ -17,9 +18,9 @@ document.getElementById("getWeatherForecast").addEventListener("click", function
             //console.log(data.list);
             //console.log(data.list[0].main.temp)
             //console.log(data.list[17].weather[0].main)
-            var unixTimestamp = data.list[0].dt;
-            var date = new Date(unixTimestamp * 1000);
-            var day = date.getDate();
+            //var unixTimestamp = data.list[0].dt;
+            //var date = new Date(unixTimestamp * 1000);
+            //var day = date.getDate();
             //console.log(day);
             let temp = [];
             for (let i = 0; i <40; i++) {
@@ -27,27 +28,27 @@ document.getElementById("getWeatherForecast").addEventListener("click", function
 
             }
 
-            console.log(temp)
-           var day1temp= temp.slice(0,8)
-           var day2temp= temp.slice(8,16)
-           var day3temp= temp.slice(16,24)
-           var day4temp= temp.slice(24,32)
-           var day5temp= temp.slice(32,40)
-            console.log(day1temp)
-            let arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length
-            console.log(Math.round(arrAvg(day5temp)))
+            console.log(temp);
+           var day1temp= temp.slice(0,8);
+           var day2temp= temp.slice(8,16);
+           var day3temp= temp.slice(16,24);
+           var day4temp= temp.slice(24,32);
+           var day5temp= temp.slice(32,40);
+            console.log(day1temp);
+            let arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
+            console.log(Math.round(arrAvg(day5temp)));
 
 
-            document.getElementById("tempday1").innerHTML=  "Today " +Math.round(arrAvg(day1temp)) +"° celcius"
-            document.getElementById("tempday2").innerHTML=  "Tomorrow " +Math.round(arrAvg(day2temp)) +"° celcius"
-            document.getElementById("tempday3").innerHTML=  "Day after Tomorrow "+Math.round(arrAvg(day3temp)) +"° celcius"
-            document.getElementById("tempday4").innerHTML=  "Next Four days "+Math.round(arrAvg(day4temp)) +"° celcius"
+            document.getElementById("tempday1").innerHTML=  "Today " +Math.round(arrAvg(day1temp)) +"° celcius";
+            document.getElementById("tempday2").innerHTML=  "Tomorrow " +Math.round(arrAvg(day2temp)) +"° celcius";
+            document.getElementById("tempday3").innerHTML=  "Day after Tomorrow "+Math.round(arrAvg(day3temp)) +"° celcius";
+            document.getElementById("tempday4").innerHTML=  "Next Four days "+Math.round(arrAvg(day4temp)) +"° celcius";
             document.getElementById("tempday4").innerHTML=  "Next Five days "+Math.round(arrAvg(day5temp)) +"° celcius"
 
         })
 });
 
-function drawWeather( d ) {
+/* function drawWeather( d ) {
                     var celcius = Math.round(parseFloat(d.main.temp));
                     var fahrenheit = Math.round(((parseFloat(d.main.temp))*1.8)+32);
                     var description = d.weather[0].description;
